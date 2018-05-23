bd1d80fc753a29e5beb6c2e4eeb3b17c1e65232e
Auston Stewart <auston@shedbuilt.net>
2018-05-22 17:03:03 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        SHED_PKG_PREFIX='/tools'
-        ;;
-    *)
-        SHED_PKG_PREFIX='/usr'
-        ;;
-esac
-./configure --prefix=$SHED_PKG_PREFIX &&
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Configure
+SHED_PKG_LOCAL_PREFIX='/usr'
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    SHED_PKG_LOCAL_PREFIX='/tools'
+fi
+./configure --prefix=$SHED_PKG_LOCAL_PREFIX &&
+
+# Build and Install
 make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install
