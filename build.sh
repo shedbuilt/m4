0205f950e26846c64aec9b35d362338d018eec65
Auston Stewart <auston@shedbuilt.net>
2018-08-14 11:20:46 -1000

    Updating for glibc 2.28

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Patch for glibc 2.28
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
 # Configure
 SHED_PKG_LOCAL_PREFIX='/usr'
 if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
